93aa14ecf1370332d45e61d5a37ce14f97a4d255
HydE <chucha1204@gmail.com>
2026-01-05 20:37:28 +0100

    create SneakerCard

---
+++ src/components/sneakerCard/index.jsx
@@ -0,0 +1,18 @@
+import { Box, Stack, Typography } from "@mui/material"
+
+function SneakerCard({img, name, price}) {
+
+  return(<Stack>
+    <img src={} alt="shoes" />
+    <Typography>{}</Typography>
+    <Stack>
+      <Stack>
+        <Typography>Price</Typography>
+        <Typography>{}</Typography>
+      </Stack>
+      <Box></Box>
+    </Stack>
+  </Stack>)
+}
+
+export default SneakerCard
